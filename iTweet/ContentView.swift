6521042dc637ab8699f4b79dc7439a7fa274a80f
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-05-09 16:56:03 +0000

    Initial Commit

---
+++ iTweet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTweet
+//
+//  Created by Emmanuel K. Nketia on 09/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iTweet/iTweetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTweetApp.swift
+//  iTweet
+//
+//  Created by Emmanuel K. Nketia on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iTweetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
